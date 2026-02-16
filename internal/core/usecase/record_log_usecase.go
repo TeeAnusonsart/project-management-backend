5c741db7f0d80385adebfc44a6f17b20c428396f
Tee <anusonsart@gmail.com>
2026-02-16 20:48:43 +0700

    fix sensor data

--- internal/core/usecase/record_log_usecase.go
+++ internal/core/usecase/record_log_usecase.go
@@ -30,6 +30,7 @@ func (uc *recordLogUsecase) Execute(deviceId string, capabilityType string,contr
         EventType: eventType,
         Value:     value,
     }
+	uc.widgetRepo.UpdateValue(widget.ID,value)
     return uc.recorder.RecordLog(logEntry)
 	// return uc.recorder.RecordLog(widget.ID, eventType, value)
 }
